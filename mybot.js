c133790ee87584ba0aae9b648733058f437c8280
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-05-01 13:32:59 -0400

    Add files via upload

--- mybot.js
+++ mybot.js
@@ -1219,8 +1219,8 @@ function setupChannel (collected, message, author) {
 							.then(col => {
 								//console.log(col);
 								const r = col.first().content.toString();
-								console.log(message.guild.role.find("name", r));
-								if(message.guild.role.find("name", r)) {
+								console.log(client.guild.role.find("name", r));
+								if(client.guild.role.find("name", r)) {
 									mongo.connect(ServerURL, function(err, db) {
 										var dbo = db.db("servers");
 										var query = { "serverID": message.guild.id };
