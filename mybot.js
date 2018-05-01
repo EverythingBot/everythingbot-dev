3f487343d70ed8558a7fed90a80199f07ea21ece
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-05-01 14:51:52 -0400

    Add files via upload

--- mybot.js
+++ mybot.js
@@ -126,7 +126,7 @@ client.on("guildMemberAdd", guild => {
 			//console.log(result);
 			if(result[0].welcomeChannel!==null){
 				guild.guild.channels.get(result[0].welcomeChannel).send(`Welcome to __**${guild.guild.name}**__, <@${guild.user.id}>!`);
-				var r = guild.guild.roles.find('name',result[0].welcomerole);
+				let r = guild.guild.roles.find("name'",result[0].welcomerole);
 				guild.addRole(r)
 					.then(console.log)
 					.catch(console.error);
