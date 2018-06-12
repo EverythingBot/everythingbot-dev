b4b81744ecbcde8c0cd6cb92b0a98727506a8e98
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-12 13:04:17 -0400

    Add files via upload

--- commands/meme.js
+++ commands/meme.js
@@ -1,5 +1,7 @@
 exports.run = (client, message, args, mongo) => {
 
+  const Discord = require("discord.js");
+
   var highest = 21;
   var url = "https://www.sheshank.com/memes/" + Math.floor((Math.random() * highest) + 1) + ".jpg";
   const embed = new Discord.RichEmbed()
