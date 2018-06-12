33e86e50c60586b22efd93725875f52c5521e138
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-12 12:00:47 -0400

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -2,5 +2,5 @@ exports.run = (client, message, args, mongo) => {
   var fs = require('fs');
   var helpMenu = fs.readFileSync('./helpmenu.txt', 'utf8');
 
-  message.channel.send(helpMenu);
+  message.channel.send({embed:{helpMenu}});
 }
