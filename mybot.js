1335091e48e13e57ca945eda082f359a627412cc
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-19 17:20:55 -0400

    Add files via upload

--- mybot.js
+++ mybot.js
@@ -172,7 +172,7 @@ client.on("message", async message => {
       } else {
         let ebot = message.guild.roles.find("name", "eBot Mute");
         var chann = message.guild.channels.array();
-        console.log(ebot);
+        console.log(ebot.id);
         for (var i = 0; i < chann.length; i++) {
           if (chann[i].type == "text") {
             chann[i].overwritePermissions({
