6a22960eae0ed962e4c374f749cbaa726c088907
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-20 18:07:37 -0400

    Add files via upload

--- commands/shibe.js
+++ commands/shibe.js
@@ -4,15 +4,15 @@ var happy = ['https://myfirstshiba.com/wp-content/uploads/2017/06/kitsu-cheeks_r
 var half = ['https://vignette.wikia.nocookie.net/doge2048/images/8/8c/Big_Doge.gif/revision/latest?cb=20161105165014'];
 
 var numCorrect;
-var users = message.guild.fetchMembers();
+var users = message.guild.members;
 var tag;
 var question = null;
 var questions = ["Is your username ","Question 2","Question 3", "Question 3"];
 var questIndex;
 
-tag = users[Math.floor(Math.random() * users.length)];
   message.channel.send("Hey! I'm the 𝕊𝕙𝕚𝕓𝕒 𝕀𝕟𝕦 𝕠𝕗 𝕎𝕚𝕤𝕕𝕠𝕞! I'm just gonna ask you some questions. Please answer with `Yes` or `No`", {files:['https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12224408/Shiba-Inu-On-White-03.jpg']}).then (msg => {
     //questIndex = Math.floor(Math.random() * questions.length);
+    tag = users[Math.floor(Math.random() * users.length)];
     questIndex=0;
     if(questIndex == 0)
       msg.channel.send("First question:\r\n" + questions[0] + tag);
