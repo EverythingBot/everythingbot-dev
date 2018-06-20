752415449423eed4271b6d7f8e8979b949b4ed3b
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-20 17:05:51 -0400

    Add files via upload

--- commands/shibe.js
+++ commands/shibe.js
@@ -1,7 +1,14 @@
 exports.run = (client, message, args, mongo) => {
+var dissapoint = ['http://www.tierversicherung.biz/gallery/images/146091549_ShibaInu1farbkombinat-Fotolia.com.jpg'];
+var happy = ['https://myfirstshiba.com/wp-content/uploads/2017/06/kitsu-cheeks_reduced.jpg'];
+var half = ['https://vignette.wikia.nocookie.net/doge2048/images/8/8c/Big_Doge.gif/revision/latest?cb=20161105165014'];
 
+var numCorrect;
+var tag = message.author.tag;
 
-  message.channel.send({files:['http://www.tierversicherung.biz/gallery/images/146091549_ShibaInu1farbkombinat-Fotolia.com.jpg']});
+  message.channel.send("Hey! I'm the Shiba Inu of Wisdom! I'm just gonna ask you some questions.").then(
+    message.channel.send("First question: " + tag);
+  )
 
 
 }
