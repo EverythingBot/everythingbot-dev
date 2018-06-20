d27c76e368a83dd9ecd8d1a27c87420ae4964e94
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-20 17:27:57 -0400

    Add files via upload

--- commands/shibe.js
+++ commands/shibe.js
@@ -6,7 +6,7 @@ var half = ['https://vignette.wikia.nocookie.net/doge2048/images/8/8c/Big_Doge.g
 var numCorrect;
 var tag = message.author.tag;
 var question = null;
-var questions = {"Is this your username?","Question 2","Question 3", "Question 3"}
+var questions = ["Is this your username?","Question 2","Question 3", "Question 3"];
 var questIndex;
 
   message.channel.send("Hey! I'm the 𝕊𝕙𝕚𝕓𝕒 𝕀𝕟𝕦 𝕠𝕗 𝕎𝕚𝕤𝕕𝕠𝕞! I'm just gonna ask you some questions. Please answer with `Yes` or `No`", {files:['https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12224408/Shiba-Inu-On-White-03.jpg']}).then (msg => {
