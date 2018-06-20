70a642e5b223212f2a1069b71411eed47332d02f
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-20 16:56:51 -0400

    Add files via upload

---
+++ commands/shibe.js
@@ -0,0 +1,7 @@
+exports.run = (client, message, args, mongo) => {
+
+
+  message.channel.send({files:['http://www.tierversicherung.biz/gallery/images/146091549_ShibaInu1farbkombinat-Fotolia.com.jpg']});
+
+
+}
