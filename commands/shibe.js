5780b667b5d56fcc3eda73de8af5256c3096f318
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-20 18:51:34 -0400

    Add files via upload

--- commands/shibe.js
+++ commands/shibe.js
@@ -15,7 +15,6 @@ var a = message.author.username;
 
   message.channel.send("Hey! I'm the 𝕊𝕙𝕚𝕓𝕒 𝕀𝕟𝕦 𝕠𝕗 𝕎𝕚𝕤𝕕𝕠𝕞! I'm just gonna ask you some questions. Please answer with `Yes` or `No`", {files:['https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12224408/Shiba-Inu-On-White-03.jpg']}).then (msg => {
     //questIndex = Math.floor(Math.random() * questions.length);
-    console.log(users[Math.floor(Math.random() * users.length)].user.username);
     tag = users[Math.floor(Math.random() * users.length)].user.username;
     questIndex=0;
     if(questIndex == 0) {
@@ -27,7 +26,7 @@ var a = message.author.username;
           errors: ['time']
         })
         .then(c => {
-          //console.log(c.first().content);
+          console.log(c.first().content);
           if(tag == a && c.first().toLowerCase() == "yes"){
             msg.channel.send ("Good job!", {files:[happy]});
           } else if(tag != a && c.first().toLowerCase() == "no"){
