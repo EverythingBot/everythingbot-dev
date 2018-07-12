f1c0a943a1aab47d79e8b8bf8a7be6bae8bf7869
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-07-12 13:01:16 -0400

    Add files via upload

--- commands/unmute.js
+++ commands/unmute.js
@@ -7,9 +7,9 @@ exports.run = (client, message, args, mongo) => {
   let member = message.mentions.members.first();
   if (!member) return message.reply("That isn't a user.");
   let muteRole = message.guild.roles.find("name", "eBot Mute");
-  if (!muteRole) return msg.reply("You haven't configured the `Muted` role yet. Please go to roles, and add `Muted` as a role. In `Muted`'s permissions, disable send messages.");
+  if (!muteRole) return message.reply("You haven't configured the `eBot Mute` role yet. Please go to roles, and add `eBot Mute` as a role. In `eBot Mute`'s permissions, disable send messages.");
 
-  if (!!member.roles.find("name", "eBot Mute")) {
+  if (!member.roles.find("name", "eBot Mute")) {
     member.removeRole(muteRole.id);
     message.channel.send(`${member.user.tag} has been unmuted.`);
   } else
