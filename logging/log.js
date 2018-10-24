e5e83e08a64e2f2d3820fdf5022d5844fd7c15f9
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-10-23 22:52:05 -0400

    Add files via upload

--- logging/log.js
+++ logging/log.js
@@ -1,42 +1,43 @@
 exports.run = (message, mongo, srvURL, clURL, type, oldMessage) => {
 
-  mongo.connect(srvURL, { useNewUrlParser: true }, function(err, db) {
+  mongo.connect(srvURL, {
+    useNewUrlParser: true
+  }, function(err, db) {
     var dbo = db.db("servers");
 
     var query = {
       "serverID": message.guild.id
     };
     dbo.collection("servers").find(query).toArray(function(err, result) {
-      if(err)
-      throw err;
+      if (err)
+        throw err;
 
-      if(type == "delete") {
-        if(message.guild.channels.get(result.logChannel)) {
+      if (type == "delete") {
+        if (message.guild.channels.get(result.logChannel)) {
 
         }
       }
 
-      if(type == "edit") {
-        if(message.guild.channels.get(result.logChannel)) {
-          var log = message.guild.channels.get(result.logChannel);
-          var loggedMessage = {
-            embed: {
-              color: 3447003,
-              description: `Message sent by ${message.author.id} was edited`,
-              fields: [
-                {
-                  name: "Old Message",
-                  value: `${oldMessage.content}`
-                },
-                {
-                  name: "New Message",
-                  value: `${message.content}`
-                }
-              ]
-            }
-          };
-          log.send(loggedMessage);
-        }
+      if (type == "edit") {
+        console.log("Message edited");
+        var l = message.guild.channels.get(result.logChannel);
+        var loggedMessage = {
+          embed: {
+            color: 3447003,
+            description: `Message sent by ${message.author.id} was edited`,
+            fields: [{
+                name: "Old Message",
+                value: `${oldMessage.content}`
+              },
+              {
+                name: "New Message",
+                value: `${message.content}`
+              }
+            ]
+          }
+        };
+        l.send(loggedMessage);
+        console.log(`Sent in ${result.logChannel}`);
       }
       db.close();
     });
