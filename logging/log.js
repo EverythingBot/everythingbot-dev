a249385cdbed6543bb0e1c0465f96874d3d93dac
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-10-26 17:37:28 -0400

    Add files via upload

--- logging/log.js
+++ logging/log.js
@@ -5,9 +5,10 @@ exports.run = (message, mongo, srvURL, clURL, type, oldMessage, oldChan, newChan
   }, function(err, db) {
     var dbo = db.db("servers");
 
-    var query = {
-      "serverID": message.guild.id
-    };
+    if (message != null)
+      var query = {
+        "serverID": message.guild.id
+      };
 
     dbo.collection("servers").find(query).toArray(function(err, result) {
       var serv = result[0];
@@ -67,57 +68,57 @@ exports.run = (message, mongo, srvURL, clURL, type, oldMessage, oldChan, newChan
         };
         l.send(loggedMessage);
       }
+      /*
+            if (type == "channelCreate") {
+              var l = client.channels.get(oldChan.id);
+              var loggedMessage = {
+                "embed": {
+                  "color": 16776960,
+                  "author": {
+                    "name": `${oldChan.client.user.username}`,
+                    "icon_url": `${oldChan.client.user.displayAvatarURL}`
+                  },
+                  "description": `Channel was Created`,
+                  "fields": [{
+                      "name": "Channel ID",
+                      "value": `${oldChan.id}`
+                    },
+                    {
+                      "name": "Channel Type",
+                      "value": `${oldChan.type}`
+                    }
+                  ],
+                  "timestamp": new Date()
+                }
+              };
+              l.send(loggedMessage);
+            }
 
-      if (type == "channelCreate") {
-        var l = client.channels.get(oldChan.id);
-        var loggedMessage = {
-          "embed": {
-            "color": 16776960,
-            "author": {
-              "name": `${oldChan.client.user.username}`,
-              "icon_url": `${oldChan.client.user.displayAvatarURL}`
-            },
-            "description": `Channel was Created`,
-            "fields": [{
-                "name": "Channel ID",
-                "value": `${oldChan.id}`
-              },
-              {
-                "name": "Channel Type",
-                "value": `${oldChan.type}`
-              }
-            ],
-            "timestamp": new Date()
-          }
-        };
-        l.send(loggedMessage);
-      }
-
-      if (type == "channelDelete") {
-        var l = client.channels.get(oldChan.id);
-        var loggedMessage = {
-          "embed": {
-            "color": 16711680,
-            "author": {
-              "name": `${oldChan.client.user.username}`,
-              "icon_url": `${oldChan.client.user.displayAvatarURL}`
-            },
-            "description": `Channel was Deleted`,
-            "fields": [{
-                "name": "Channel ID",
-                "value": `${oldChan.id}`
-              },
-              {
-                "name": "Channel Type",
-                "value": `${oldChan.type}`
-              }
-            ],
-            "timestamp": new Date()
-          }
-        };
-        l.send(loggedMessage);
-      }
-
+            if (type == "channelDelete") {
+              var l = client.channels.get(oldChan.id);
+              var loggedMessage = {
+                "embed": {
+                  "color": 16711680,
+                  "author": {
+                    "name": `${oldChan.client.user.username}`,
+                    "icon_url": `${oldChan.client.user.displayAvatarURL}`
+                  },
+                  "description": `Channel was Deleted`,
+                  "fields": [{
+                      "name": "Channel ID",
+                      "value": `${oldChan.id}`
+                    },
+                    {
+                      "name": "Channel Type",
+                      "value": `${oldChan.type}`
+                    }
+                  ],
+                  "timestamp": new Date()
+                }
+              };
+              l.send(loggedMessage);
+            }
+      */
       db.close();
     });
   });
